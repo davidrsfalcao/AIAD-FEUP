8da8ce3f3754c66f06ff6c7a3cf8eeb9693142d0
David Falcão <davidrsfalcao@gmail.com>
2018-10-17 09:07:39 +0100

    add agents

---
+++ src/agents/PostMan.java
@@ -0,0 +1,16 @@
+package agents;
+
+import elements.Point;
+import jade.core.Agent;
+
+public class PostMan extends Agent {
+
+    Point position;
+
+
+
+
+
+
+
+}
---
+++ src/agents/PostOffice.java
@@ -0,0 +1,12 @@
+package agents;
+
+import elements.Point;
+import jade.core.Agent;
+
+public class PostOffice extends Agent {
+
+    Point position;
+
+
+
+}
