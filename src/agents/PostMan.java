833613bb3a5d0e2f3f826498184db9c13454447c
Pedro <pedro21fcp@gmail.com>
2018-10-24 23:03:44 +0100

    quicksort for Postman's Orders working

--- src/agents/PostMan.java
+++ src/agents/PostMan.java
@@ -1,7 +1,6 @@
 package agents;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
 
 import communication.handlers.postMan.Handler;
@@ -35,6 +34,52 @@ public class PostMan extends Agent {
         int comsuption = rnd.nextInt(51)+100;
         vehicle = new Vehicle(capacity,comsuption);
         instance = this;
+        
+        //teste
+//        Point p1= new Point(10,10);
+//        Point p2 = new Point(2,2);
+//        Point p3 = new Point(10,11);
+//        Order o1 = new Order(p1,10);
+//        Order o2 = new Order(p2,20);
+//        Order o3 = new Order(p3,30);
+//        this.orders.add(o1);
+//        this.orders.add(o2);
+//        this.orders.add(o3);
+//        System.out.println("Before Sort: "+ this.orders);
+//        sortOrders(0,this.orders.size());
+//        System.out.println("After Sort: "+ this.orders);
+        
+    }
+    
+    public void sortOrders(int start,int end) {
+    	//No inicio passar start como 0 e end como this.orders.size();
+    	Order aux;
+    	int i=start,j = end-1;
+    	Point pivot =  this.orders.get(((start+end)/2)).getDestiny();
+    	while(i <= j) {
+    		while( (this.orders.get(i).getDestiny().getDistance(position)) < pivot.getDistance(position) && i < end ) {
+    			i++;
+    		}
+    		while( (this.orders.get(j).getDestiny().getDistance(position)) > pivot.getDistance(position) && j > start) {
+    			j--;
+    		}
+    		if(i <= j) {
+    			aux = this.orders.get(i);
+    			this.orders.set(i, this.orders.get(j));
+    			this.orders.set(j,aux);
+    			i++;
+    			j--;
+    		}
+    		
+    	}
+    	
+    	if(j > start) {
+    		sortOrders(start,j+1);
+    	}
+    	if(i < end) {
+    		sortOrders(i,end);
+    	}
+    	
     }
     
     public void setup() {
