741ffb9394621349e2e9a4280709439d0d38a094
David Falcão <davidrsfalcao@gmail.com>
2018-10-24 09:44:13 +0100

    update communication

--- src/agents/PostMan.java
+++ src/agents/PostMan.java
@@ -1,12 +1,12 @@
 package agents;
 
+import communication.messages.HelloMessage;
 import elements.Point;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
-import messages.TestMessage;
 
 import java.util.concurrent.TimeUnit;
 
@@ -32,24 +32,24 @@ public class PostMan extends Agent {
 
         @Override
         public void action() {
-            ACLMessage msg = new ACLMessage(TestMessage.INFORM);
 
-            msg.addReceiver(postOffice);
-            msg.setContent("ola");
-            send(msg);
+            send(new HelloMessage(name, postOffice).toACL());
 
-            ACLMessage receive = receive();
-            if(receive != null) {
-                System.out.println("[POSTMAN] " + receive.getContent());
-                System.out.println(msg);
-                ACLMessage reply = receive.createReply();
-                reply.setPerformative(ACLMessage.INFORM);
-                reply.setContent("Ola!");
+            while(true){
+                ACLMessage receive = receive();
+                if(receive != null) {
+                    System.out.println("[POSTMAN] " + receive.getContent());
+                    ACLMessage reply = receive.createReply();
+                    reply.setPerformative(ACLMessage.INFORM);
+                    reply.setContent("Ola!");
 
-                send(reply);
-            } else {
-                block();
+                    send(reply);
+                } else {
+                    block();
+                }
             }
+
+
 
 
 
